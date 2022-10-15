27601243f817896e334af05d42df48e451fa3caa
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-10-15 16:48:27 +0900

    Sequerilzeから既存のデータベースから情報を取得するようにしてみた

--- serverless/src/index.ts
+++ serverless/src/index.ts
@@ -1,6 +1,59 @@
 import awsLambdaFastify from '@fastify/aws-lambda';
 import fastify from 'fastify';
 import twilio from 'twilio';
+import { Sequelize, Model, DataTypes } from 'sequelize';
+
+const sequelize = new Sequelize({
+  host: "localhost",
+  dialect: 'mysql',
+  database: "hackathon_portal_development",
+  username: "root",
+  password: null,
+  logging: true,
+});
+
+class PromoteUser extends Model {}
+PromoteUser.init({
+  // Modelの定義 & カラム情報の定義
+  id: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.STRING,
+  },
+  screen_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  state: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+    allowNull: false,
+  },
+  follower_count: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+    allowNull: false,
+  },
+  follow_count: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  // Table名を直接指定する場合はこうする
+  tableName: "promote_users",
+  // createdAtやupdatedAtを指定したくない場合はtimestamps: falseを指定する
+  timestamps: false,
+});
+
 const tilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const VoiceResponse = twilio.twiml.VoiceResponse;
 
@@ -10,6 +63,9 @@ const fromPhoneNumber = '+...';
 const app = fastify();
 
 app.get('/', async (request, reply) => {
+  const promote_user = await PromoteUser.findOne({ where: { id: 1597223525203449 } })
+  console.log(promote_user)
+  console.log(promote_user.id)
   return { hello: 'world' };
 });
 
