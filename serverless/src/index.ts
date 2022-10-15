947c92cca936025d100345621a0ca2f45f40358d
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-10-15 15:11:51 +0900

    twilioから電話をかけられるようにしました

--- serverless/src/index.ts
+++ serverless/src/index.ts
@@ -2,11 +2,29 @@ import awsLambdaFastify from '@fastify/aws-lambda';
 import fastify from 'fastify';
 import twilio from 'twilio';
 const tilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const VoiceResponse = twilio.twiml.VoiceResponse;
 
 const app = fastify();
 
 app.get('/', async (request, reply) => {
   return { hello: 'world' };
+});
+
+app.get('/call', async (request, reply) => {
+  const twiml = new VoiceResponse();
+  twiml.say(
+    {
+      language: 'ja-JP',
+      voice: 'woman',
+    },
+    'オッス!!オラ悟空!!',
+  );
+  const callResult = await tilioClient.calls.create({
+    twiml: twiml.toString(),
+    from: '+...',
+    to: '+...'
+  })
+  return callResult;
 });
 
 export const handler = awsLambdaFastify(app);
