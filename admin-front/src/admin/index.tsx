eabf317555b9f92c297afc90a569ab5344ef52d0
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-10-16 15:21:36 +0900

    URLの変更

--- admin-front/src/admin/index.tsx
+++ admin-front/src/admin/index.tsx
@@ -2,7 +2,8 @@ import { Admin, Resource, ListGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { VitalCreate } from './sensor-vital-create';
 
-const dataProvider = jsonServerProvider("https://p2e3m40ce5.execute-api.ap-northeast-1.amazonaws.com/production/admin");
+//const dataProvider = jsonServerProvider("http://localhost:3000/dev");
+const dataProvider = jsonServerProvider("https://p2e3m40ce5.execute-api.ap-northeast-1.amazonaws.com/production");
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
