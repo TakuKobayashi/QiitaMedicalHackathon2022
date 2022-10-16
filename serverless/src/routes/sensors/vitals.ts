7441095caf9e584b02769a47e0e29065e4f95683
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-10-16 12:16:05 +0900

    バイタルデータの登録処理の作成

--- serverless/src/routes/sensors/vitals.ts
+++ serverless/src/routes/sensors/vitals.ts
@@ -1,12 +1,24 @@
+import crypto from "crypto"
+import { setupFireStore } from '../../common/firestore';
+const firestore = setupFireStore();
+
+const tableName = "patient_vital";
+
 export async function sensorsVitalRouter(app, opts): Promise<void> {
   app.get('/', async (req, res) => {
     res.send('hello sensor');
   });
-  app.get('/list', async (req, res) => {
-    res.send('hello admin');
-  });
   app.post('/record', async (req, res) => {
     console.log(req.body);
-    res.send('ok');
+    const recordId = crypto.randomBytes(12).toString('hex')
+    const currentDoc = await firestore.collection(tableName).doc(recordId);
+    // vital_type = 1 心拍数
+    // 計測するバイタルデータのtypeと値を基本的に記録する
+    const newData = {
+      ...req.body,
+      patient_id: "test-patient-id", id: recordId, created_at: new Date().getTime()
+    }
+    await currentDoc.set(newData);
+    return newData
   });
 }
